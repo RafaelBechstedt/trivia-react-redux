eee079782eef988703f33d2ef8f1c8276b1b4afe
Leonardo Kepler Mesquita <leokepler@gmail.com>
2022-10-07 21:24:39 -0300

    Requisito 4 :white_check_mark:

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,3 +1,4 @@
+import { waitFor } from '@testing-library/react';
 import { screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import App from "../App";
@@ -6,19 +7,24 @@ import renderWithRouterAndRedux from "./helpers/renderWithRouterAndRedux";
 describe('', () => {
     it('', async() => {
         const { history } = renderWithRouterAndRedux(<App />);
-        const inputEmail = screen.getByTestId("input-gravatar-email")
-        const inputName = screen.getByTestId("input-player-name")
-        const btnPlay = screen.getByTestId("btn-play")
-        const btnSettings = screen.getByTestId("btn-settings")
-
-        expect(btnPlay).toBeDisabled()
-        userEvent.type(inputEmail,'teste@teste.com')
-        userEvent.type(inputName,'teste')
-        expect(btnPlay).not.toBeDisabled()
-        userEvent.click(btnPlay)
-        const { pathname } = history.location;
-        await expect(pathname).toBe('/game')
-
+        const inputEmail = screen.getByTestId("input-gravatar-email");
+        const inputName = screen.getByTestId("input-player-name");
+        const btnPlay = screen.getByTestId("btn-play");
+        expect(btnPlay).toBeDisabled();
+        userEvent.clear(inputEmail);
+        userEvent.clear(inputName);
+        userEvent.type(inputEmail,'teste@teste.com');
+        userEvent.type(inputName,'teste');
+        expect(btnPlay).not.toBeDisabled();
+        userEvent.click(btnPlay);
+        await waitFor(() => { expect(history.location.pathname).toBe('/game');})
+    })
+    
+    it('', () => {
+        const { history } = renderWithRouterAndRedux(<App />);
+        const btnSettings = screen.getByTestId("btn-settings");
+        userEvent.click(btnSettings);        
+        expect(history.location.pathname).toBe('/settings');
     })
 
 })
